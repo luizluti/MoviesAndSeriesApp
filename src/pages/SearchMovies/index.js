e22561258c84c573c9a32c90e19d7a27511284b5
Luiz Pires <luizluti@outlook.com>
2020-09-07 10:45:08 -0300

    refactor SearchMovies styles to styled components

--- src/pages/SearchMovies/index.js
+++ src/pages/SearchMovies/index.js
@@ -1,13 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  Image
-} from 'react-native'
-
-import { Container } from './styles'
+import { Container, FlatList, Image, NoImage, NoImageText, Title, Description, HR } from './styles'
 import Axios from 'axios'
 
 const SearchMovies = ({ route }) => {
@@ -25,85 +17,28 @@ const SearchMovies = ({ route }) => {
   return (
     <Container>
       <FlatList
-        style={styles.results}
         data={movies}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View style={styles.results}>
-
+          <>
             {item.poster_path
               ? <Image
                 source={{ uri: `https://image.tmdb.org/t/p/w200${item.poster_path}` }}
-                style={{
-                  height: 400,
-                  width: 250,
-                  alignSelf: 'center',
-                  marginTop: 20
-                }}
-                // resizeMode="cover"
               />
-              : <View style={{
-                width: '100%',
-                height: 200,
-                backgroundColor: '#445565',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 20
-              }}>
-                <Text style={{
-                  fontSize: 24
-                }}>Sem Imagem</Text>
-              </View> }
+              : <NoImage>
+                <NoImageText>Sem Imagem</NoImageText>
+              </NoImage> }
 
-            <Text style={styles.heading}>{item.title}</Text>
+            <Title>{item.title}</Title>
 
-            <Text style={{
-              color: '#909090',
-              fontSize: 16,
-              margin: 10
-            }}>{item.overview}</Text>
-            {/* <Text style={{ margin: 10, color: '#FFF' }}>Nota: {item.vote_average}</Text> */}
-          </View>
+            <Description>{item.overview}</Description>
+
+            <HR/>
+          </>
         )}
       />
     </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#223343',
-    alignItems: 'center',
-    paddingTop: 50,
-    paddingHorizontal: 20
-  },
-  searchbox: {
-    fontSize: 20,
-    fontWeight: '300',
-    padding: 20,
-    width: '100%',
-    backgroundColor: '#FFF',
-    borderRadius: 8,
-    marginBottom: 10
-  },
-  results: {
-    flex: 1,
-    borderBottomColor: '#909090',
-    borderBottomWidth: 1
-  },
-  movie: {
-    flex: 1,
-    width: '100%'
-  },
-  heading: {
-    color: '#FFF',
-    fontSize: 24,
-    fontWeight: '700',
-    marginTop: 10,
-    alignSelf: 'center'
-    // backgroundColor: '#445565'
-  }
-})
 
 export default SearchMovies
